af766a5f15a4e772c286b2b2d353b9302c3765ed
alaeddineth <theljanialaeddine@gmail.com>
2023-12-10 16:36:20 +0100

    Course Project

---
+++ JavaScript/Data_Types/script.js
@@ -0,0 +1,42 @@
+// Number
+let age = 25;
+
+// String
+let name = "John";
+
+// Boolean
+let isStudent = true;
+
+// Array
+let colors = ["red", "green", "blue"];
+
+// Object
+let person = {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 30,
+    isStudent: false
+};
+
+// Null
+let car = null;
+
+// Undefined
+let book;
+
+// Symbol
+let id = Symbol("unique");
+
+// Displaying values in the console
+console.log("Number:", age);
+console.log("String:", name);
+console.log("Boolean:", isStudent);
+console.log("Array:", colors);
+console.log("Object:", person);
+console.log("Null:", car);
+console.log("Undefined:", book);
+console.log("Symbol:", id);
+var num = 0 ;
+num = num+1;
+num++;
+console.log(num);
---
+++ WorkShop-JavaScript/script.js
@@ -0,0 +1,6 @@
+// Grab code snippets from each section in the rendered page to the left
+// paste it in this file to execute
+// click the run button on top to check results in the console to the left
+// Good Luck ✊ 
+
+console.log("I've got this 💪")
